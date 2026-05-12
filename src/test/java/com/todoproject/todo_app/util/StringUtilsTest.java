df5aa237727edb9564171cf79ec580cd9ab6dd18
alexeigrigoriev <gralex555@gmail.com>
2026-05-12 19:38:41 +0300

    test: add basic tests for StringUtils.isBlank method

--- src/test/java/com/todoproject/todo_app/util/StringUtilsTest.java
+++ src/test/java/com/todoproject/todo_app/util/StringUtilsTest.java
@@ -1,4 +1,31 @@
 package com.todoproject.todo_app.util;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class StringUtilsTest {
+
+    @Test
+    void isBlankShouldReturnTrueWhenInputIsNull() {
+        String input  = null;
+
+        boolean result = StringUtils.isBlank(input);
+        assertTrue(result);
+    }
+
+    @Test
+    void isBlankShouldReturnTrueWhenInputIsEmptyString() {
+        String input = "";
+        boolean result = StringUtils.isBlank(input);
+        assertTrue(result);
+
+    }
+
+    @Test
+    void isBlankShouldReturnFalseWhenInputContainsText() {
+        String input = "hello";
+        boolean result = StringUtils.isBlank(input);
+        assertFalse(result);
+    }
+
 }
