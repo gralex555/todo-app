96e916ff13a6c5a545ee3dd88359c552b3c7562a
alexeigrigoriev <gralex555@gmail.com>
2026-06-18 22:54:44 +0300

    test: update tests after Kafka integration

--- src/main/java/com/todoproject/todo_app/config/KafkaConsumerConfig.java
+++ src/main/java/com/todoproject/todo_app/config/KafkaConsumerConfig.java
@@ -18,7 +18,7 @@ import java.util.Map;
 @EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
-    @Value("${spring.kafka.bootstrap-servers}")
+    @Value("${spring.kafka.bootstrap-servers:localhost:9092}")
     private String bootstrapServers;
 
     @Bean
